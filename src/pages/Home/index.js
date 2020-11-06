7c0391ebaf2cb3f907aebe137cc7608227b475ac
elpmoraes <elpmoraes@gmail.com>
2020-11-05 23:11:39 -0300

    Comentários

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,18 +9,18 @@ export default function Home() {
     const navegacao = useNavigation();
 
     function irCronometro() {
-        navegacao.navigate('Cronometro')
-    
-    
+        navegacao.navigate('Cronometro')  
     }
     
   return (
     <View>
-      <Text>Home Screen</Text>
+          <Text>Home Screen</Text>
+          {/* Através de função anonima */}
       <TouchableOpacity style={styles.botao} onPress={() => navegacao.navigate('Treinos')}>
         <Text>Treinos</Text>
           </TouchableOpacity>
-          
+                
+    {/* chamando funcao externa */}
           <TouchableOpacity style={styles.botao} onPress={irCronometro}>
         <Text>Cronometro</Text>
       </TouchableOpacity>
@@ -31,7 +31,7 @@ export default function Home() {
 const styles = StyleSheet.create({
   botao: {
     alignItems: 'center',
-    backgroundColor: '#DDDDDD',
+    backgroundColor: '#F0E68C',
     padding: 10,
   },
 });
