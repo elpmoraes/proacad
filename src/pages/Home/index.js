79703e96ef1bb009acd2dc4b53b845eff8813870
Felipe Moraes <lipew_cf@hotmail.com>
2020-11-09 17:24:28 -0300

    Adicionado Icons e Melhorias no layout

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,37 +1,78 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
-
+import FontAwesome from 'react-native-vector-icons/FontAwesome5';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ImageBackground,
+} from 'react-native';
+import {ActivityIndicator} from 'react-native-paper';
 
 
 export default function Home() {
+  const navegacao = useNavigation();
 
-    const navegacao = useNavigation();
+  function irCronometro() {
+    navegacao.navigate('Cronometro');
+  }
+  const image = {
+    uri:
+      'https://midias.agazeta.com.br/2020/03/17/coronavirus-academias-ficarao-fechadas-em-linhares--209864-article.jpg',
+  };
 
-    function irCronometro() {
-        navegacao.navigate('Cronometro')  
-    }
-    
   return (
     <View>
-          <Text>Home Screen</Text>
-          {/* Através de função anonima */}
-      <TouchableOpacity style={styles.botao} onPress={() => navegacao.navigate('Treinos')}>
-        <Text>Treinos</Text>
-          </TouchableOpacity>
-                
-    {/* chamando funcao externa */}
-          <TouchableOpacity style={styles.botao} onPress={irCronometro}>
-        <Text>Cronometro</Text>
-      </TouchableOpacity>
+      <ImageBackground source={image} imageStyle={{opacity:0.3}} style={styles.image}>
+        <View style={styles.titulo}>
+          <FontAwesome name="dumbbell" size={40} color={'#7FDCF8FF'}>
+            <Text> Pró ACAD </Text>
+            <FontAwesome
+              name="dumbbell"
+              size={40}
+              color={'#7FDCF8FF'}></FontAwesome>
+          </FontAwesome>
+        </View>
+        {/* Através de função anonima */}
+        <TouchableOpacity
+          style={styles.botao}
+          onPress={() => navegacao.navigate('Treinos')}>
+          <FontAwesome name="fire-alt" size={30} color={'white'}>
+            <Text> Treinos</Text>
+          </FontAwesome>
+        </TouchableOpacity>
+
+        {/* chamando funcao externa */}
+        <TouchableOpacity style={styles.botao} onPress={irCronometro}>
+          <FontAwesome name="stopwatch-20" size={30} color={'white'}>
+            <Text> Cronômetro </Text>
+          </FontAwesome>
+        </TouchableOpacity>
+      </ImageBackground>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  titulo: {
+    backgroundColor: '#5191FDFF',
+
+    alignItems: 'center',
+    padding: 20,
+    margin: 3,
+    borderBottomWidth: 3,
+    borderBottomColor: '#7FDCF8FF',
+  },
   botao: {
     alignItems: 'center',
-    backgroundColor: '#F0E68C',
-    padding: 10,
+    backgroundColor: '#FF7A9AFF',
+    padding: 20,
+    margin: 20,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+
   },
 });
--- src/pages/Treinos/index.js
+++ src/pages/Treinos/index.js
@@ -1,13 +1,19 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, TouchableOpacity, Alert} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, ImageBackground} from 'react-native';
 import {FlatList} from 'react-native-gesture-handler';
 import {Datatable, DataTable} from 'react-native-paper';
-
+import FontAwesome from 'react-native-vector-icons/FontAwesome5';
 
 export default function Treino() {
 const [opcao, setOpcao] = useState();
 
-const opcoes = [{key: 'A'}, {key: 'B'}, {key: 'C'}, {key: 'D'}, {key: 'E'}];
+const opcoes = [
+  {key: 'A', n: 'dice-one'},
+  {key: 'B', n: 'dice-two'},
+  {key: 'C', n: 'dice-three'},
+  {key: 'D', n: 'dice-four'},
+  {key: 'E', n: 'dice-five'},
+];
 
 const A = [
   {treino: 'flexao', repeticao: '5x 12-10-8-6-4'},
@@ -22,7 +28,7 @@ const E = [{treino: 'flexao', repeticao: '200'}];
 const [treino, setTreino] = useState(A);
 
 function definirTreino(opcao) {
-  alert(opcao);
+  alert("Treino '" + opcao + "' ativado.");
   switch (opcao) {
     case 'A':
       setTreino(A);
@@ -54,9 +60,13 @@ function definirTreino(opcao) {
           data={opcoes}
           renderItem={({item}) => (
             <TouchableOpacity
-              style={style.botao}
+            
               onPress={() => definirTreino(item.key)}>
-              <Text style={style.fonteBotao}>{item.key}</Text>
+              <Text style={style.fonteBotao}>
+                <FontAwesome name={item.n} size={45}>
+                  {item.key}
+                </FontAwesome>
+              </Text>
             </TouchableOpacity>
           )}
         />
@@ -68,7 +78,10 @@ function definirTreino(opcao) {
               <Text style={style.headerTable}>Exercício</Text>
             </DataTable.Title>
             <DataTable.Title numeric>
-              <Text style={style.headerTable}>Repetição</Text>
+              <Text style={style.headerTable}>
+                {' '}
+                <FontAwesome name="edit" size={25}></FontAwesome>Repetição
+              </Text>
             </DataTable.Title>
           </DataTable.Header>
 
@@ -93,25 +106,30 @@ function definirTreino(opcao) {
 const style = StyleSheet.create({
   body: {flex: 1},
   header: {
-    backgroundColor: '#CB9898FF',
+    backgroundColor: '#FF7A9AFF',
     flex: 1,
     flexDirection: 'row',
   },
 
   content: {backgroundColor: '#62A0C8FF', flex: 6},
-  treinos: {backgroundColor: '#FF002BFF'},
+  treinos: {backgroundColor: '#FF7A9AFF'},
 
   foot: {backgroundColor: '#FFF', flex: 0.8},
   botao: {
-    backgroundColor: '#8777B4',
-    borderWidth: 1,
-    width: 90,
-    height: 90,
+    backgroundColor: '#53777AFF',
+
+    width: 95,
+    height: 75,
     alignItems: 'center',
-    padding: 8,
+    margin: 1,
   },
-  fonteBotao: {color: '#F7F4F4FF', fontWeight: 'bold', fontSize: 25},
-  headerTable: {backgroundColor: 'red', fontSize: 25, color: 'white'},
+  fonteBotao: {
+    color: '#F7F4F4FF',
+    padding: 5,
+    fontWeight: 'bold',
+    fontSize: 35,
+  },
+  headerTable: {backgroundColor: '#FF7A9AFF', fontSize: 25, color: 'white'},
 
   exercicioText: {fontSize: 28, color: 'white'},
   repeticaoText: {fontSize: 23, color: 'white'},
