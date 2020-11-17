f76ac11d119a536c5a598617b48809b477373a53
Felipe Moraes <lipew_cf@hotmail.com>
2020-11-17 00:36:06 -0300

    AlterarPerfil (finalizado)

--- src/pages/Treinos/index.js
+++ src/pages/Treinos/index.js
@@ -1,4 +1,52 @@
-import React, {useState} from 'react';
+  /*   case 'A':
+        treino = [{ exec: "Flexão", repeticao: "4x 10" },
+          { exec: "Supino Maq", repeticao: "5x 12-10-8-6-4" },
+          { exec: "Crucifixo Maq", repeticao: "6x 12" },
+          { exec: "Voador", repeticao: "4x 10" },
+          { exec: "Fly Declinado", repeticao: "4x 10" },
+          { exec: "ABS Maq", repeticao: "4x 20" },
+          { exec: "ABS Infra", repeticao: "4x 20" },
+          { exec: "Prancha", repeticao: "4x 2min"},
+        ]
+        break;
+        case 'B':
+          treino = [{ exec: "Puxada Aberta", repeticao: "5x 12-10-8-6-4" },
+          { exec: "Remada Curvada Pronada", repeticao: "5x 12-10-8-6-4" },
+          { exec: "Remada Aberta (chão)", repeticao: "5x 12" },
+          { exec: "Serrote", repeticao: "4x 10" },
+          { exec: "Pulldown", repeticao: "6x 12" },
+          { exec: "Encolhimento c/ barra", repeticao: "5x 20" },
+          { exec: "Encolhimento c/ halter", repeticao: "5x 20" },
+          { exec: "Extensão/Flexão de Punho", repeticao: "5x 20"},
+        ]
+        break;
+        case 'C':
+          treino = [{ exec: "Agachamento Maq", repeticao: "5x 5" },
+          { exec: "Mesa Flexora", repeticao: "5x 8" },
+          { exec: "Cadeira Extensora", repeticao: "4x 8" },
+          { exec: "Perdigueiro", repeticao: "3x 20s" },
+          { exec: "Superman", repeticao: "3x 20s" },
+          { exec: "Stiff", repeticao: "4x 12" }]
+      break;
+        case 'D':
+          treino = [{ exec: "Desenv. Militar c/ barra ", repeticao: "5x 5" },
+          { exec: "Desenv. Arnold", repeticao: "4x 8" },
+          { exec: "Elevacão Frontal", repeticao: "5x 12" },
+          { exec: "Evevação Lateral", repeticao: "5x 12" },
+            { exec: "Remada Alta Maq", repeticao: "5x12" },
+            { exec: "Crucifixo Inverso", repeticao: "5x12" },
+          { exec: "Panturrilha", repeticao: "5x 20" }]
+        break;
+        case 'E':
+          treino = [{ exec: "Rosca Direta", repeticao: "4x 12-10-8-6" },
+          { exec: "Triceps Testa na maq", repeticao: "4x 12-10-8-6" },
+          { exec: "Rosca Alternada", repeticao: "4x 8" },
+          { exec: "Triceps Corda", repeticao: "4x 8" },
+            { exec: "Rosca Concentrada", repeticao: "4x 10" },
+            { exec: "Rosca Martelo", repeticao: "6x 12" },
+  { exec: "Triceps Coice", repeticao: "6x 12" }]
+          /*
+import React, { useState } from 'react';
 import {View, Text, StyleSheet, TouchableOpacity, ImageBackground} from 'react-native';
 import {FlatList} from 'react-native-gesture-handler';
 import {Datatable, DataTable} from 'react-native-paper';
